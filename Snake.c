d8126fab7864c41cf6a50a4e8f6bd36eefd48bc3
m1ad3n <stanimiroviccmladen@gmail.com>
2022-05-21 11:32:42 +0200

    small changes

--- Snake.c
+++ Snake.c
@@ -1,113 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <conio.h>
 #include <time.h>
+#include <windows.h>
 
-int main(int argc, char** argv[])
+char map[10][20], player = 254, fruit = 3, body_img = 178;
+int i, y, cordX = 4, cordY = 9, fruitX, fruitY, score = 0, key = 0x0, smer = 0x0;
+
+DWORD key_input()
 {
-	srand(time(0));
+    while (key != 0x71)
+    {
+        if (key == 0x50 && smer != 0x2)
+            smer = 0x1;
+        else if (key == 0x48 && smer != 0x1)
+            smer = 0x2;
+        else if (key == 0x4D && smer != 0x4)
+            smer = 0x3;
+        else if (key == 0x4B && smer != 0x3)
+            smer = 0x4;
 
-	char map[10][20], player = 254, fruit = 3;
-	int i, y, cordX = 4, cordY = 9, fruitX, fruitY, key = 0x0, score = 0;
+        key = _getch();
+    }
+    return 0;
+}
 
-	for (i = 0; i < (sizeof(map) / sizeof(map[0])); i++)
-	{
-		for (y = 0; y < (sizeof(map[0]) / sizeof(char)); y++)
-		{
-			map[i][y] = ' ';
-		}
-	}
+DWORD screen_ptr()
+{
+    for (i = 0; i < (sizeof(map) / sizeof(map[0])); i++)
+    {
+        for (y = 0; y < (sizeof(map[0]) / sizeof(char)); y++)
+        {
+            map[i][y] = ' ';
+        }
+    }
 
-	fruitX = rand() % (sizeof(map) / sizeof(map[0]));
-	fruitY = rand() % (sizeof(map[0]) / sizeof(char));
-	map[fruitX][fruitY] = fruit;
+    fruitX = rand() % (sizeof(map) / sizeof(map[0]));
+    fruitY = rand() % (sizeof(map[0]) / sizeof(char));
+    map[fruitX][fruitY] = fruit;
 
-	while (key != 0x71)
-	{
-		if (key == 0x50)
-		{
-			map[cordX][cordY] = ' ';
+    while (key != 0x71)
+    {
+        map[cordX][cordY] = ' ';
 
-			if (cordX != 9) cordX++;
-			else
-			{
-				printf("\nYou lost%c\n", 7);
-				break;
-			}
-		}
-		else if (key == 0x48)
-		{
-			map[cordX][cordY] = ' ';
+        switch (smer)
+        {
+        case 0x1:
+            if (cordX != 9) cordX++;
+            else { printf("\nYou lost%c\n", 7); exit(0); }
 
-			if (cordX != 0) cordX--;
-			else
-			{
-				printf("\nYou lost%c\n", 7);
-				break;
-			}
-		}
-		else if (key == 0x4D)
-		{
-			map[cordX][cordY] = ' ';
+            break;
+        case 0x2:
+            if (cordX != 0) cordX--;
+            else { printf("\nYou lost%c\n", 7); exit(0); }
 
-			if (cordY != 19) cordY++;
-			else
-			{
-				printf("\nYou lost%c\n", 7);
-				break;
-			}
-		}
-		else if (key == 0x4B)
-		{
-			map[cordX][cordY] = ' ';
+            break;
+        case 0x3:
+            if (cordY != 19) cordY++;
+            else { printf("\nYou lost%c\n", 7); exit(0); }
 
-			if (cordY != 0) cordY--;
-			else
-			{
-				printf("\nYou lost%c\n", 7);
-				break;
-			}
-		}
+            break;
+        case 0x4:
+            if (cordY != 0) cordY--;
+            else { printf("\nYou lost%c\n", 7); exit(0); }
 
-		if (cordX == fruitX && cordY == fruitY)
-		{
-			score++;
+            break;
+        }
 
-			while (1)
-			{
-				fruitX = rand() % (sizeof(map) / sizeof(map[0]));
-				fruitY = rand() % (sizeof(map[0]) / sizeof(char));
+        if (cordX == fruitX && cordY == fruitY)
+        {
+            while (cordX == fruitX && cordY == fruitY)
+            {
+                fruitX = rand() % (sizeof(map) / sizeof(map[0]));
+                fruitY = rand() % (sizeof(map[0]) / sizeof(char));
+            }
 
-				if (cordX != fruitX && cordY != fruitY)
-					break;
-			}
+            map[fruitX][fruitY] = fruit;
+            score++;
+        }
 
-			map[fruitX][fruitY] = fruit;
-		}
+        map[cordX][cordY] = player;
 
-		// assign player to screen
-		map[cordX][cordY] = player;
+        // clear the console
+        system("cls");
 
-		// clear the console
-		system("cls");
+        printf("%c", 201);
+        for (i = 0; i < 20; i++)
+            printf("%c", 205);
+        printf("%c", 187);
 
-		// print out the map
-		printf(" ____________________");
-		for (i = 0; i < (sizeof(map) / sizeof(map[0])); i++)
-		{
-			printf("\n|");
-			for (y = 0; y < (sizeof(map[0]) / sizeof(char)); y++)
-			{
-				printf("%c", map[i][y]);
-			}
-			printf("|");
-		}
+        for (i = 0; i < (sizeof(map) / sizeof(map[0])); i++)
+        {
+            printf("\n%c", 186);
+            for (y = 0; y < (sizeof(map[0]) / sizeof(char)); y++)
+            {
+                printf("%c", map[i][y]);
+            }
+            printf("%c", 186);
+        }
 
-		printf("\n --------------------");
-		printf("\n\nScore: %d\n", score);
+        printf("\n%c", 200);
+        for (i = 0; i < 20; i++)
+            printf("%c", 205);
 
-		// get keyboard input
-		key = _getch();
-	}
+        printf("%c", 188);
+        printf("\n\nScore: %d\n", score);
+
+        Sleep(500);
+    }
+
+    return 0;
+}
+
+int main()
+{
+    srand(time(0));
+
+    HANDLE thread1, thread2;
+
+    // make threads
+    thread1 = CreateThread(NULL, 0, key_input, NULL, 0, NULL);
+    thread2 = CreateThread(NULL, 0, screen_ptr, NULL, 0, NULL);
+
+    // wait for them to finish
+    WaitForSingleObject(thread1, INFINITE);
+    WaitForSingleObject(thread2, INFINITE);
 }
